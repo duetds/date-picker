4bdb09d1555c24a7602dccfe24d2a0c038d6d1a0
Viljami Salminen <viljami.salminen@gmail.com>
2020-08-13 10:20:52 +0300

    Fixes Firefox issues with padding

--- src/components/duet-date-picker/duet-date-picker.tsx
+++ src/components/duet-date-picker/duet-date-picker.tsx
@@ -600,8 +600,8 @@ export class DuetDatePicker implements ComponentInterface {
                       aria-hidden="true"
                       fill="currentColor"
                       xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
+                      width="21"
+                      height="21"
                       viewBox="0 0 24 24"
                     >
                       <path d="M14.71 15.88L10.83 12l3.88-3.88c.39-.39.39-1.02 0-1.41-.39-.39-1.02-.39-1.41 0L8.71 11.3c-.39.39-.39 1.02 0 1.41l4.59 4.59c.39.39 1.02.39 1.41 0 .38-.39.39-1.03 0-1.42z" />
@@ -618,8 +618,8 @@ export class DuetDatePicker implements ComponentInterface {
                       aria-hidden="true"
                       fill="currentColor"
                       xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
+                      width="21"
+                      height="21"
                       viewBox="0 0 24 24"
                     >
                       <path d="M9.29 15.88L13.17 12 9.29 8.12c-.39-.39-.39-1.02 0-1.41.39-.39 1.02-.39 1.41 0l4.59 4.59c.39.39.39 1.02 0 1.41L10.7 17.3c-.39.39-1.02.39-1.41 0-.38-.39-.39-1.03 0-1.42z" />
